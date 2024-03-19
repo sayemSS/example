6086d99477635c145b6834ff563c072edb9f921b
sayemSS <sayembiswas783@gmail.com>
2024-03-20 03:51:29 +0600

    new

---
+++ main.py
@@ -0,0 +1,17 @@
+def calculateMoney(ticket_count):
+   
+    if ticket_count < 0:
+        return "ইনপুট সংখ্যাটি অস্বীকার্য"
+
+    
+    total_income = ticket_count * 120
+    total_expense = 500 + (8 * 50)
+    profit = total_income - total_expense
+
+    return profit
+
+
+ticket_count = int(input("টিকেট সংখ্যা দিন: "))
+
+
+print("বেকার ভাইর কাছে বাকি থাকা টাকার পরিমাণ:", calculateMoney(ticket_count))
